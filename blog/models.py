52fbc31cd23c3866da5c9c2a7d58ca05739b3c18
asus <sahruz708@gmail.com>
2024-03-16 10:49:01 +0500

    added str for models and connected media for url

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,9 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.id} : {self.author}'
+
 
 class LikePost(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
